5e572806684c1392e98f3629744024ab7474e7a6
Jimmol-Singh <40445351+Jimmol-Singh@users.noreply.github.com>
2020-09-20 23:07:10 -0400

    Update main.java

--- insertingElementsIntoArray/main.java
+++ insertingElementsIntoArray/main.java
@@ -4,10 +4,10 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		DVD[] dvdCollection = new DVD[4];
+            DVD[] dvdCollection = new DVD[4];
 		
 		
-		DVD avengersDVD = new DVD("The Avengers", 2012, "Joss Whedon");
+	    DVD avengersDVD = new DVD("The Avengers", 2012, "Joss Whedon");
 	    dvdCollection[0] = avengersDVD;
 	    System.out.println(dvdCollection[0]);
 	    
